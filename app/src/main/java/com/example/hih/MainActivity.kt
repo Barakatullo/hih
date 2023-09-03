d307317516e529e86a998e1f66bbeeafa5badf83
Barakat <adam1baranov@gmail.com>
2023-09-03 16:10:08 +0300

    create project

---
+++ app/src/main/java/com/example/hih/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hih
+// 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
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
